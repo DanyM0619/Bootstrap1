574f00e2ba0a6957db5e8569a6487403f4c9f40b
Dany <daedmegu@gmail.com>
2021-09-12 09:53:20 -0500

    Se Realizo el cuadro basico a través de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgbdTableBasic } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    NgbdTableBasic,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NgbdTableBasic]
+  
 })
 export class AppModule { }
+
